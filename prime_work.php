49a60e80eb06c5727109773dfad9b32670706196
shuntakeuchi <takeuchishun89@gmail.com>
2016-04-07 01:21:37 +0000

    first commit

---
+++ prime_work.php
@@ -0,0 +1,11 @@
+<?php
+for($i=2;$i<=100;$i++){
+  if($i==2 || $i==3 || $i==5 || $i==7){
+    echo $i."<br>";
+    continue;
+  }elseif($i%2==0 || $i%3 ==0 || $i%5==0 || $i%7==0){
+    continue;
+  }
+  echo $i."<br>";
+}
+?>
