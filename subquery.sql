72e02772e0c8b96d8b10d1e8bad322086587f389
shuntakeuchi <takeuchishun89@gmail.com>
2016-04-29 08:35:36 +0000

    サブクエリの詳細の準備

---
+++ subquery.sql
@@ -0,0 +1,58 @@
+create database subquery_db;
+
+use subquery_db;
+
+grant all on subquery_db.* to testuser@localhost identified by '9999';
+
+create table sales(
+member_id int primary key auto_increment,
+sale int,
+month int
+);
+insert into sales (member_id,sale,month) values
+  (1 , 75 , 4),
+  (2 , 200 , 5),
+  (3 , 15 , 6),
+  (4 , 700 , 5),
+  (5 , 672 , 4),
+  (6 , 56 , 8),
+  (7 , 231 , 9),
+  (8 , 459 , 8),
+  (9 , 8 , 7),
+  (10 , 120 , 4);
+
+
+create table members(
+member_id int primary key auto_increment,
+name varchar(32)
+);
+
+create table age(
+member_id int primary key auto_increment,
+age int
+);
+
+insert into members (name) values
+  ("Tanaka"),
+  ("Sato"),
+  ("Suzuki"),
+  ("Tsuchiya"),
+  ("Yamada"),
+  ("Sasaki"),
+  ("Harada"),
+  ("Takahashi"),
+  ("Nishida"),
+  ("Nakada");
+
+insert into age (age) values
+  (24),
+  (25),
+  (47),
+  (55),
+  (39),
+  (26),
+  (43),
+  (33),
+  (24),
+  (20);
+ 1. 最大の売上を出した社員の名前 2. 売上の平均以上を達成した社員の名前 3. 30代以下の社員が達成した売上の合計
